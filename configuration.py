004dcbad38ebdfdf1453a44db9d7b8eec4f91129
Flavio <flavio_nz@outlook.com>
2025-09-12 15:34:58 -0600

    Segunda entrega a revisión con las últimas correcciones realizadas

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-260b0c84-249b-423a-b4b5-cbc9883ef5ae.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-09d35617-0467-4e48-868f-c0184251bc77.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
